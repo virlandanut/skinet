3330c5410eb5de68c0d19965ad95f320268c8d74
virlandanut <virlan.danut@outlook.com>
2024-09-30 14:12:32 +0300

    Filters, sort and search in Angular

---
+++ client/src/app/features/shop/product-item/product-item.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Product } from '../../../shared/models/product';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+} from '@angular/material/card';
+import { CurrencyPipe } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
+
+@Component({
+  selector: 'app-product-item',
+  standalone: true,
+  imports: [
+    MatCard,
+    MatCardContent,
+    CurrencyPipe,
+    MatCardActions,
+    MatIcon,
+    MatButton,
+  ],
+  templateUrl: './product-item.component.html',
+  styleUrl: './product-item.component.scss',
+})
+export class ProductItemComponent {
+  @Input() product?: Product;
+}
